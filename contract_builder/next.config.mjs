ba564c7630d5546c0561d1a09f346122f971b31c
Chad <chadnuttall1@gmail.com>
2025-09-29 16:20:49 -0400

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   contract_builder/next.config.mjs

--- contract_builder/next.config.mjs
+++ contract_builder/next.config.mjs
@@ -1,4 +1,13 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
+
+
 
 export default nextConfig;
