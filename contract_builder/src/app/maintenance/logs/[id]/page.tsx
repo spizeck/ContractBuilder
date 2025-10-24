1ab2fa90f75d28b840c8810f3e8e1cf07ee057fc
Chad <chadnuttall1@gmail.com>
2025-10-24 13:28:09 -0400

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   contract_builder/src/app/maintenance/logs/[id]/page.tsx

--- contract_builder/src/app/maintenance/logs/[id]/page.tsx
+++ contract_builder/src/app/maintenance/logs/[id]/page.tsx
@@ -1,11 +1,11 @@
 import LogForm from "@/components/maintenance/LogForm";
 
-// export default function EditLogPage(props: any) {
-//   const { id } = props.params;
-//   return <LogForm id={id} />;
-// }
-
-export default async function EditLogPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params
-  return <LogForm id={id} />
+export default function EditLogPage(props: any) {
+  const { id } = props.params;
+  return <LogForm id={id} />;
 }
+
+// export default async function EditLogPage({ params }: { params: Promise<{ id: string }> }) {
+//   const { id } = await params
+//   return <LogForm id={id} />
+// }
